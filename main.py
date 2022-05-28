23ce445e18e00563d2e3a022f61953e9e9221b0e
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2022-05-28 09:17:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+@app.route('/pythonlogin/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
